baa4d0bde8568e56ad9fe818f99a1918a28ce1db
adityayuga <yugaapik@gmail.com>
2017-01-18 10:02:21 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Hapi = require('hapi')
+const server = new Hapi.Server()
+server.connection({ port: 3000, host: 'localhost' })
+
+// Print some information about the incoming request for debugging purposes
+server.ext('onRequest', function (request, next) {
+    var ip = request.headers['x-forwarded-for'] || request.info.remoteAddress
+    console.log(ip, request.path, request.query)
+    next()
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply({ message: 'Welcome to Kamus Indonesia-Chinese API' }).code(200)
+  }
+})
+
+// Start the server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
