3ef03944019531376e321a461273d0fe70aaa546
adityayuga <yugaapik@gmail.com>
2017-01-18 16:17:20 +0700

    fix error middleware

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const server = new Hapi.Server()
 server.connection({ port: 3000, host: 'localhost' })
 
 // Print some information about the incoming request for debugging purposes
-server.ext('onRequest', function (request, next) {
+server.ext('onRequest', function (request, reply) {
     var ip = request.headers['x-forwarded-for'] || request.info.remoteAddress
     console.log(ip, request.path, request.query)
-    next()
+    return reply.continue()
 });
 
+// index
 server.route({
   method: 'GET',
   path: '/',
