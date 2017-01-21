5ee53731a5dcff2570fa7aa29c79c3e149abd198
adityayuga <yugaapik@gmail.com>
2017-01-21 19:59:43 +0700

    update server.js and user plugin

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    server: {
+      host: 'localhost',
+      port: 3000
+    },
+    database: {
+        host: 'localhost',
+        port: 27017,
+        db: 'kamusAPI',
+        username: '',
+        password: ''
+    },
+    key: {
+        privateKey: '37LvDSm4XvjYOh9Y',
+        tokenExpiry: 1 * 30 * 1000 * 60 //1 hour
+    }
+    /*,
+    email: {
+        username: "test@test.com",
+        password: "password",
+        accountName: "Cronj",
+        verifyEmailUrl: "verifyEmail"
+    }*/
+};
--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 'use strict';
 
 const Hapi = require('hapi')
+const Db = require('./config/db')
+const Config = require('./config/config')
+const Moment = require('moment')
+
+var app = {};
+app.config = Config;
+
 const server = new Hapi.Server()
-server.connection({ port: 3000, host: 'localhost' })
+server.connection({ port: app.config.server.port, host: app.config.server.host })
 
 // Print some information about the incoming request for debugging purposes
 server.ext('onRequest', function (request, reply) {
     var ip = request.headers['x-forwarded-for'] || request.info.remoteAddress
     console.log(ip, request.path, request.query)
     return reply.continue()
-});
+})
+
+var privateKey = app.config.key.privateKey;
+var ttl = app.config.key.tokenExpiry;
+
+// Validate function to be injected
+var validate = function(token, callback) {
+    // Check token timestamp
+    var diff = Moment().diff(Moment(token.iat * 1000))
+    if (diff > ttl) {
+        return callback(null, false)
+    }
+    callback(null, true, token)
+}
 
 // index
 server.route({
@@ -20,11 +40,31 @@ server.route({
   }
 })
 
-// Start the server
-server.start((err) => {
+// Plugins
+server.register([{
+    register: require('hapi-auth-jwt')
+}, {
+    register: require('./users'),
+}], {
+    routes: {
+        prefix: '/api/v1'
+    }
+}, (err) => {
 
     if (err) {
-        throw err;
+        throw err
     }
-    console.log(`Server running at: ${server.info.uri}`);
-});
+
+    server.auth.strategy('token', 'jwt', {
+        validateFunc: validate,
+        key: privateKey
+    })
+})
+
+// Start the server
+server.start((err) => {
+    if (err) {
+        throw err
+    }
+    console.log(`Server running at:`, server.info.uri)
+})
