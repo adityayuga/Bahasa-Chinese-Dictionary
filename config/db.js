29d0c89aa57e7907cbc2f85bb54d6c1f0ab96846
adityayuga <yugaapik@gmail.com>
2017-02-06 11:12:13 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const Mongoose = require('mongoose')
 const config = require('./config')
 
-Mongoose.connect(config.database)
+//Mongoose.connect(config.database)
+Mongoose.connect("mongodb://memey:aksiomaekaristi@ds135519.mlab.com:35519/kamus")
 const db = Mongoose.connection
 db.on('error', console.error.bind(console, 'connection error'))
 db.once('open', function callback() {
